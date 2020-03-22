c614ce75c95e8787e50560c08eaeff2ca18737d0
yuanyexi <yuanyexi1991@gmail.com>
2020-03-22 19:02:56 +0800

    Start vuex store for project

--- src/main.js
+++ src/main.js
@@ -1,14 +1,33 @@
 import Vue from "vue";
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import router from "./router/index.js";
+import store from "./store/index.js";
+import BaseIcon from "@/components/BaseIcon";
+
+Vue.component("BaseIcon", BaseIcon);
 
 Vue.config.productionTip = false;
+
+const requireComponent = require.context(
+  "./components",
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+);
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName);
+
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, "$1"))
+  );
+
+  Vue.component(componentName, componentConfig.default || componentConfig);
+});
 
 new Vue({
   router,
   store,
-  render: function(h) {
-    return h(App);
-  }
+  render: h => h(App)
 }).$mount("#app");
---
+++ src/views/EventCreate.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState(["user", "categories"])
+  }
+};
+</script>
